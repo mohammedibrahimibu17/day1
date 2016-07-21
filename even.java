c8b77593eacfea7347839ff9029a150bd9d562be
mohammedibrahimibu17 <mohammedibrahimibu17ab@gmail.com>
2016-07-21 18:29:21 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class Two
+{
+int num;
+Scanner obj=new Scanner(System.in);
+num=obj.next;
+if(num%==0)
+System.out.print("Even");
+else
+System.out.print("odd");
+}
